5904478f8046e9de86d93ea799f85de9a0dafea7
Jindřich Káňa <jindrich.kana@elostech.cz>
2018-05-03 10:58:02 +0200

    logging atd

--- setupdb.sql
+++ setupdb.sql
@@ -12,8 +12,10 @@ CREATE TABLE user_review (
 CREATE USER 'hodnoceni'@'%' IDENTIFIED BY 'hodnoceni';
 CREATE USER 'hodnoceni'@'localhost' IDENTIFIED BY 'hodnoceni';
 CREATE USER 'hodnoceni'@'127.0.0.1' IDENTIFIED BY 'hodnoceni';
+CREATE USER 'hodnoceni'@'gateway' IDENTIFIED BY 'hodnoceni';
 GRANT ALL ON reviews.* TO 'hodnoceni'@'%';
 GRANT ALL ON reviews.* TO 'hodnoceni'@'127.0.0.1';
 GRANT ALL ON reviews.* TO 'hodnoceni'@'locahost';
+GRANT ALL ON reviews.* TO 'hodnoceni'@'gateway';
 
 FLUSH PRIVILEGES;
